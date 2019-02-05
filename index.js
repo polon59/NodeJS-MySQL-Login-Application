f9b1268146501319bef4286883cdc1e40711945c
polon59 <przemyslaw.polczak@gmail.com>
2019-02-05 17:55:37 +0100

    Change port value in 'index.js'

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) =>{
-    res.send("hello world");
+    res.send("hello");
 });
 
 app.get("/api/courses", (req, res) =>{
     res.send([1,2,3]);
 });
 
-app.listen(8000, () => {console.log('listening on port 8000')});
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {console.log(`listening on port ${port}`)});

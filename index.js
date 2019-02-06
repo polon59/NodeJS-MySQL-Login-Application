5cace09838e6df8cdd3a992d2d43e3908ef14605
polon59 <przemyslaw.polczak@gmail.com>
2019-02-06 14:50:49 +0100

    Add text sent with response 404

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    {id: 1, name:"course1"},
+    {id: 2, name:"course2"},
+    {id: 3, name:"course3"},
+    {id: 4, name:"course4"},
+]
+
 app.get("/", (req, res) =>{
     res.send("hello");
 });
 
 app.get("/api/courses", (req, res) =>{
-    res.send([1,2,3]);
+    res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) =>{
+    const result = courses.find(c => c.id === parseInt(req.params.id));
+    if (!result){
+        res.status(404).send("This course does not exist.");
+    }else{
+        res.send(result);
+    } 
 });
 
 const port = process.env.PORT || 8000;
